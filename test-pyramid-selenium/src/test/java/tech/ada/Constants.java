9f7f8189295f7f2e353f976c7194a5dd9b605d2b
Daniel Rodrigues <danielrodrigues452796@hotmail.com>
2025-11-11 19:55:03 -0300

    refactor(data): Cria Constants.java para dados de teste

--- test-pyramid-selenium/src/test/java/tech/ada/Constants.java
+++ test-pyramid-selenium/src/test/java/tech/ada/Constants.java
@@ -2,5 +2,19 @@ package tech.ada;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://localhost:8081";
+    // Dados para REQ004 (Caminho Feliz)
+    public static final String VALID_FIRST_NAME = "Selenium";
+    public static final String VALID_LAST_NAME = "Testes";
+    public static final String VALID_EMAIL = "selenium.happy@ada.com";
+    public static final String VALID_USERNAME = "seleniumhappy";
+    public static final String VALID_PASSWORD = "Password12345";
+
+    // Dados para REQ006 (Usuário Existente) - Deve ser o mesmo do seu banco de dados
+    public static final String EXISTING_USERNAME = "usuarioexistente";
+    public static final String EXISTING_EMAIL = "existente@ada.com";
+
+    // Dados para REQ005 (Validação - Dados Inválidos)
+    public static final String INVALID_SHORT_DATA = "a"; // < 2 chars
+    public static final String INVALID_EMAIL = "emailinvalido";
+    public static final String INVALID_SHORT_PASSWORD = "short"; // < 8 chars
 }
